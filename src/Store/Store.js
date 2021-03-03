b115b50910712f980ccee3b8371e251108862d6e
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-03 22:33:15 +0330

    finish redux structure

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,11 +1,11 @@
-import {createStore , applyMiddleWare , compose} from 'redux';
+import {createStore , applyMiddleware , compose} from 'redux';
 import Reducer from '../Reducers/index';
 import thunk from 'redux-thunk';
 
 const Store = createStore(
     Reducer,
     compose(
-        applyMiddleWare(thunk),
+        applyMiddleware(thunk),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import Store from './Store/Store';
 
-render(<App /> ,  document.getElementById("root"));
+render(
+    <Provider store={Store}>
+        <App />
+    </Provider>
+    ,  document.getElementById("root")
+);
