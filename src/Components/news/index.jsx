80906a1baabb0f5f0db35810bde44e223948d5e3
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-05 06:46:06 +0330

    v

--- src/Components/news/index.jsx
+++ src/Components/news/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NewsDiv from './news-div';
 
+import "./Compiled/news.css";
+
 const NewContainer = ()=>{
     return(
         <main id="main-news">
-            
+
         </main>
     );
 };
---
+++ src/Components/news/notNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotNews = ()=>{
+    return(
+        <div className="not-news">
+
+        </div>
+    );
+}
+
+export default NotNews;
